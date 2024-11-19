71687c217809fd1b07172e12f0ed52b18e1239fe
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-11-19 15:18:50 +0100

    Add setup.py for packaging and dependencies of Kooked Kopf operator

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kooked-kopf',
+    version='2.0.0',
+    description='Kopf operator for managing and deploying Kooked applications',
+    url='https://github.com/kooked-ch/operator',
+    packages=find_packages(),
+    install_requires=[
+        'kopf==1.37.2',
+        'kubernetes==31.0.0'
+    ],
+)
