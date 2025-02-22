8a84b29e4c567e5e4f55357dc1a69b0434302c5b
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-22 15:01:24 +0100

    Bump operator version to 3.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kooked-kopf',
-    version='3.0.25',
+    version='3.1.0',
     description='Kopf operator for managing and deploying Kooked applications',
     url='https://github.com/kooked-ch/operator',
     packages=find_packages(),
